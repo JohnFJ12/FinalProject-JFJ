f2a47759a8efbd3df4c0905dccb56c9c06046981
JohnFJ12 <150172953+JohnFJ12@users.noreply.github.com>
2023-11-23 03:08:24 -0500

    Beginning of Interface Development

--- Project Cortex.py
+++ Project Cortex.py
@@ -1,1 +1,10 @@
+import tkinter 
 
+userinterface = tkinter.Tk()
+userinterface.attributes("-fullscreen", True)
+
+
+welcomeprompt = tkinter.Label(text = "Hi I am Cortex")
+welcomeprompt.place(x = 100, y =userinterface.winfo_width()-1000)
+
+userinterface.mainloop()
