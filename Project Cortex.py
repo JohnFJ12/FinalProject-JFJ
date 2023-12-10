4a72cfaea50a399942f3ca7aafc2f3c66270d97c
JohnFJ12 <150172953+JohnFJ12@users.noreply.github.com>
2023-12-10 01:59:57 -0500

    Update Project Cortex.py

--- Project Cortex.py
+++ Project Cortex.py
@@ -4,10 +4,29 @@ import typingsimu
 userinterface = tkinter.Tk()
 userinterface.attributes("-fullscreen", True)
 
-cortexTextbox = tkinter.Text(x = 100, y = 100, width= 150, height = 25, background= "gray", font = ("Times New Roman", 30), yscrollcommand=True, borderwidth= 10)
+cortexTextbox = tkinter.Text(width= 150, height = 15, background= "blue", font = ("Times New Roman", 30), yscrollcommand=True, borderwidth= 10)
 cortexTextbox.pack()
-uiTextbox = tkinter.Text(x = 100, y = 100, width= 150, height = 25, background= "white",font = ("Times New Roman", 30), borderwidth=10)
+uiTextbox = tkinter.Text(width= 150, height = 10, background= "black",font = ("Times New Roman", 30), borderwidth=10)
 uiTextbox.pack()
+uiTxtbutton = tkinter.Button(userinterface, text= "Enter",font = ("Times New Roman", 30), borderwidth=10, command=lambda: calculatorcallers())
+uiTxtbutton.pack()
+typingsimu.typing("Hi My Name is Cortex your assistant in how can I help you today?",cortexTextbox)
 
-typingsimu.typing("Hi My Name is Cortex your assistant in anything Calc 3 how can I help you today?",cortexTextbox)
+def integralui():
+     pass
+
+def dotproductui():
+     pass
+
+def crossproductui():
+     pass
+
+
+
+def calculatorcallers():
+    uihelper = uiTextbox.get(1.0, "end-1c")
+    if "joke" in uihelper.lower():
+         return typingsimu.typing("Alright we done to much Calc 3 your right, Lets lighten the mood. ",cortexTextbox)
+ 
+
 userinterface.mainloop()
